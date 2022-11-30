03425b9b82676be39b5cba84ea07d59677d1df3b
dspineda <dspinedao@gmail.com>
2022-11-29 23:53:10 -0500

    feat/main in go

--- src/main.go
+++ src/main.go
@@ -2,8 +2,40 @@ package main
 
 import "fmt"
 
+func dobleRetorno(a int) (int, int) {
+	return 5, 6
+}
+
 func main() {
+	// Declaracion de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+	fmt.Println("Hello, World!", pi, pi2)
 
-	fmt.Println("Hello, World!")
+	// Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+	
+	fmt.Println(base, altura, area)	
 
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool 
+
+	fmt.Println(a,b,c,d)
+
+	// Area cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("🚀 ~ file: main.go:29 ~ funcmain ~ areaCuadrado", areaCuadrado)
+	fmt.Println(areaCuadrado)
+
+	valor1, valor2 := dobleRetorno(5)
+	fmt.Println(valor1, valor2)
+
+	value1, _ := dobleRetorno(5)
+	fmt.Println(value1)
 }
